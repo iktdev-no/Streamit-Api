e00a37692ea6950a3e229775e293ee8f0d0dba9b
bskjon <bskjon@outlook.com>
2021-07-04 16:41:40 +0200

    Continue should now work

--- src/main/kotlin/no/iktdev/streamitapi/controllers/ProgressController.kt
+++ src/main/kotlin/no/iktdev/streamitapi/controllers/ProgressController.kt
@@ -6,8 +6,10 @@ import no.iktdev.streamitapi.database.progress
 import no.iktdev.streamitapi.helper.progressHelper
 import no.iktdev.streamitapi.services.ProgressService
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.web.bind.annotation.*
+import javax.management.Query.and
 
 @RestController
 class ProgressController
@@ -69,7 +71,60 @@ class ProgressController
     }
 
 
+    @GetMapping("/progress/{guid}/continue")
+    fun getContinue(@PathVariable guid: String):  List<ProgressTable>
+    {
+        val _progress: MutableList<ProgressTable> = mutableListOf()
 
+        transaction {
+            val msx = progress.season.max().alias(progress.season.name)
+            val mex = progress.episode.max().alias(progress.episode.name)
+
+            val episodeTable = progress.slice(
+                mex,
+                progress.title
+            ).selectAll().groupBy(progress.title).alias("episodeTable")
+
+            val seasonTable = progress.slice(
+                msx,
+                progress.title
+            ).selectAll().groupBy(progress.title).alias("seasonTable")
+
+            /*
+SELECT * FROM progress
+INNER JOIN (
+    SELECT MAX(episode) as episode, title FROM progress GROUP BY title
+) AS res
+INNER JOIN (
+    SELECT MAX(season) as season, title FROM progress GROUP BY title
+) AS ses
+ON progress.title = res.title
+AND progress.title = ses.title
+AND progress.season = ses.season
+AND progress.episode = res.episode;
+             */
+
+            progress
+                .join(episodeTable, JoinType.INNER)
+                {
+                    progress.title eq episodeTable[progress.title] and(progress.episode eq  episodeTable[mex])
+                }
+                .join(seasonTable, JoinType.INNER)
+                {
+                    progress.title eq seasonTable[progress.title] and(progress.season eq seasonTable[msx])
+                }
+                .select { progress.played.isNotNull() }
+                .andWhere { progress.guid eq guid }
+                .orderBy(progress.played, SortOrder.DESC)
+                .limit(10)
+                .mapNotNull {
+                    _progress.add(ProgressTable.fromRow(it))
+                }
+
+
+        }
+        return _progress
+    }
 
 
 
