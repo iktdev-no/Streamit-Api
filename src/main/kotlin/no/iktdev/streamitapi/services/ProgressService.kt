33c7ad5e38d54cee09a6e45cbda654a8545e6489
bskjon <bskjon@outlook.com>
2021-08-24 17:26:35 +0200

    Added check on progress 0

--- src/main/kotlin/no/iktdev/streamitapi/services/ProgressService.kt
+++ src/main/kotlin/no/iktdev/streamitapi/services/ProgressService.kt
@@ -28,13 +28,25 @@ class ProgressService
             run {
                 if (found != null) {
                     var _progress = progressMovie.progress
+                    var _duration = progressMovie.progress
+                    var _played = progressMovie.played
+
                     if (found[progress.progress] > progressMovie.progress) {
                         println("Re-using DB data " + progressMovie.title)
                         println("Reason: Present progress is larger than passed..")
                         _progress = found[progress.progress]
-
                     }
-                    var _played = progressMovie.played
+
+                    if (found[progress.duration] > progressMovie.duration) {
+                        if (progressMovie.duration == 0) {
+                            println("Duration passed is 0! Please verify that the application is recording as intended!")
+                        } else {
+                            println("Duration passed is less than data present in database, please verify that the content is the same!")
+                        }
+                        _duration = found[progress.duration]
+                    }
+
+
                     if (found[progress.played] > progressMovie.played) {
                         _played = found[progress.played]
                     }
@@ -42,7 +54,7 @@ class ProgressService
                     progress.update({ progress.id eq found[progress.id] })
                     {
                         it[this.progress] = _progress
-                        it[this.duration] = progressMovie.duration
+                        it[this.duration] = _duration
                         it[this.played] = _played
                         it[this.video] = progressMovie.video ?: ""
                     }
@@ -79,11 +91,21 @@ class ProgressService
                     val video = it.video ?: found[progress.video]
                     val title = it.title
                     var _progress = it.progress
+                    var _duration = it.duration
 
                     if (found[progress.progress] > it.progress) {
                         println("Re-using DB data " + it.title + " - S" + it.season + " E" + it.episode)
                         println("Reason: Present progress is larger than passed..")
                         _progress = found[progress.progress]
+                    }
+
+                    if (found[progress.duration] > it.duration) {
+                        if (it.duration == 0) {
+                            println("Duration passed is 0! Please verify that the application is recording as intended!")
+                        } else {
+                            println("Duration passed is less than data present in database, please verify that the content is the same!")
+                        }
+                        _duration = found[progress.duration]
                     }
 
                     var _played = it.played
@@ -94,7 +116,7 @@ class ProgressService
                     progress.update({ progress.id eq found[progress.id] }) { table ->
                         table[this.video] = video
                         table[this.progress] = _progress
-                        table[this.duration] = it.duration
+                        table[this.duration] = _duration
                         table[this.played] = _played
                         table[this.title] = title
                     }
