d83736eb2747082642f621bc9f373e396abb46d2
Brage Skjonborg <brage.skjonborg@hotmail.no>
2022-01-20 22:49:14 +0100

    Fixed check

--- src/main/kotlin/no/iktdev/streamitapi/services/operation/CatalogItemRemovalService.kt
+++ src/main/kotlin/no/iktdev/streamitapi/services/operation/CatalogItemRemovalService.kt
@@ -50,7 +50,7 @@ class CatalogItemRemovalService {
             LoggerFactory.getLogger(javaClass.simpleName).error(errorText)
             return Response(false, errorText)
         } else {
-            if (file.delete()) { LoggerFactory.getLogger(javaClass.simpleName).info("Failed to delete file, none changes made.."); return Response(false, "Failed to delete file..") }
+            if (!file.delete()) { LoggerFactory.getLogger(javaClass.simpleName).info("Failed to delete file, none changes made.."); return Response(false, "Failed to delete file..") }
             val subtitleFile = getSubtitleItemPath(file.nameWithoutExtension)
             if (subtitleFile != null && subtitleFile.exists()) {
                 subtitleFile.delete()
@@ -84,7 +84,7 @@ class CatalogItemRemovalService {
             return Response(false, "File not found, please see console")
         }
 
-        if (file.delete()) { LoggerFactory.getLogger(javaClass.simpleName).info("Failed to delete file, none changes made.."); return Response(false, "Failed to delete file/directory") }
+        if (!file.delete()) { LoggerFactory.getLogger(javaClass.simpleName).info("Failed to delete file, none changes made.."); return Response(false, "Failed to delete file/directory") }
 
         val subtitleFile = getSubtitleItemPath(collection)
         if (subtitleFile != null && subtitleFile.exists()) {
