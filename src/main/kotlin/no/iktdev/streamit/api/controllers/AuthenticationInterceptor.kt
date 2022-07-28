ccb55de17179120bf696bb9aa7e6706dad92a4b9
Brage Skjonborg <brage.skjonborg@hotmail.no>
2022-07-28 19:41:47 +0200

    Updated logging

--- src/main/kotlin/no/iktdev/streamit/api/controllers/AuthenticationInterceptor.kt
+++ src/main/kotlin/no/iktdev/streamit/api/controllers/AuthenticationInterceptor.kt
@@ -15,13 +15,12 @@ import javax.servlet.http.HttpServletResponse
 @Component
 class AuthenticationInterceptor: HandlerInterceptor, Authy() {
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        LoggerFactory.getLogger(javaClass.simpleName).info("Using Auth Interceptor")
         val validation = (handler as HandlerMethod).method.getAnnotation(Authentication::class.java)
         return if (validation == null) {
             super.preHandle(request, response, handler)
         } else {
             val value = request.getHeader("Authorization")
-            when(validation.mode) {
+            val valid = when(validation.mode) {
                 AuthenticationModes.SOFT -> {
                     val isValid = isValid(value)
                     if (!isValid)
@@ -40,6 +39,8 @@ class AuthenticationInterceptor: HandlerInterceptor, Authy() {
                     false
                 }
             }
+            if (valid) LoggerFactory.getLogger(javaClass.simpleName).info("Auth interceptor accepted") else LoggerFactory.getLogger(javaClass.simpleName).error("Auth interceptor rejected!")
+            valid
         }
     }
 }
