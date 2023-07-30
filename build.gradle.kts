902ff46c3488e04e5855b6c8d431bbdf12705733
Brage <bskjon@outlook.com>
2023-07-30 12:58:25 +0200

    Changed version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "no.iktdev"
-version = "0.0.5.4-SNAPSHOT"
+version = "0.0.6.0-SNAPSHOT"
 base.archivesBaseName = "streamit.api"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
