3a80da7f3fd19344362127f94e747f3c9701ce90
bskjon <bskjon@outlook.com>
2025-01-01 20:21:20 +0100

    Logging

--- src/main/kotlin/no/iktdev/streamit/api/controllers/AuthenticationInterceptor.kt
+++ src/main/kotlin/no/iktdev/streamit/api/controllers/AuthenticationInterceptor.kt
@@ -2,12 +2,15 @@ package no.iktdev.streamit.api.controllers
 
 import no.iktdev.streamit.api.controllers.annotations.Authentication
 import no.iktdev.streamit.api.controllers.annotations.AuthenticationModes
+import no.iktdev.streamit.api.getRequestersIp
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.method.HandlerMethod
 import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler
+import java.util.stream.Collectors
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -15,7 +18,18 @@ import javax.servlet.http.HttpServletResponse
 @Component
 class AuthenticationInterceptor: HandlerInterceptor, Authy() {
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        val validation = (handler as HandlerMethod).method.getAnnotation(Authentication::class.java)
+        val validation = try {
+            if (handler is HandlerMethod)
+                (handler as HandlerMethod).method.getAnnotation(Authentication::class.java)
+            else null
+        } catch (e: Exception) {
+            e.printStackTrace()
+            val url = request.requestURL.toString()
+            val queryParams = request.queryString
+            val body = request.reader.lines().collect(Collectors.joining(System.lineSeparator()))
+            log.error { "Error report:\n\tSource:${request.getRequestersIp()}\n\tUrl:$url\n\tQuery params:$queryParams\n\tBody:$body" }
+            null
+        }
         return if (validation == null) {
             super.preHandle(request, response, handler)
         } else {
